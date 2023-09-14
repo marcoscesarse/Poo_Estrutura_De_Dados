1dea18a04798e4cfc8e2ae5c8167b0bec4527b45
marcoscesarse <marcos.cesar.se@gmail.com>
2023-09-14 20:42:38 -0300

    Add files via upload

---
+++ HelloWorld/src/br/com/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package br.com.HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int valor;
+		valor = 10;
+		valor = valor * 12;
+		
+		System.out.println(valor);
+	}
+
+}
